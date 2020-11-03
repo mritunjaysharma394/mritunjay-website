7a9ead0f6ca0465f563f5f74c6698b41f4ff1386
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-11-04 00:05:23 +0530

    Fixed a broken link in bio

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -4,7 +4,7 @@ import { Styled } from "theme-ui"
 
 export default () => (
   <Fragment>
-    Words by <Styled.a href="http://example.com/">Mritunjay Sharma</Styled.a>.
+    Words by <Styled.a href="https://twitter.com/mritunjay394">Mritunjay Sharma</Styled.a>.
     <br />
     Junior Year Computer Science Engineering Student | Sometimes writing code and sometimes poems | Chaotic and Poetic | 20 y.o | Connect with me on <Styled.a href = "https://twitter.com/mritunjay394"> Twitter </Styled.a> :)
   </Fragment>
