e95c091051a37650acc82cb2f46b312b24617088
Mritunjay Sharma <mritunjaysharma394@gmail.com>
2020-11-04 00:10:49 +0530

    gatsby cli update

--- src/gatsby-theme-blog/components/bio-content.js
+++ src/gatsby-theme-blog/components/bio-content.js
@@ -6,6 +6,6 @@ export default () => (
   <Fragment>
     Words by <Styled.a href="https://twitter.com/mritunjay394">Mritunjay Sharma</Styled.a>.
     <br />
-    Junior Year Computer Science Engineering Student | Sometimes writing code and sometimes poems | Chaotic and Poetic | 20 y.o | Connect with me on <Styled.a href = "https://twitter.com/mritunjay394"> Twitter </Styled.a> :)
+    Junior Year Computer Science Engineering Student| Based in Lucknow, India | Sometimes writing code and sometimes poems | Chaotic and Poetic | 20 y.o | Connect with me on <Styled.a href = "https://twitter.com/mritunjay394"> Twitter </Styled.a> :)
   </Fragment>
 )
